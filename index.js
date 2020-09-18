288fb74639688eec34c7104dec1c38bbe3a20167
zhx-zhang <60555905+zhx-zhang@users.noreply.github.com>
2020-09-18 16:25:25 -0400

    Update index.js

--- index.js
+++ index.js
@@ -9,17 +9,6 @@ server.use(plugins.queryParser())
 
 server.get('/', (req,res,next) => {
 
-    // make sure the required parameters are passed
-    if (!req.query['forward_to']) {
-        return next(new errors.BadRequestError('Missing forward_to parameter'))
-    }
-
-    if (req.query['arg_format']) {
-        if (req.query['arg_format']!='json' && req.query['arg_format']!='qs') {
-            return next(new errors.BadRequestError('Invalid arg_format parameter.'))
-        }
-    } 
-
     // collect the query variables  
     // and store in JSON format 
     var props = {}
@@ -30,27 +19,14 @@ server.get('/', (req,res,next) => {
         }
     }
 
-    // remove the forward_to & arg_format from
-    // props and what's left over will be the
-    // rest of the query variables
-    let url_scheme = props['forward_to']
-    delete props['forward_to']
-
-    let arg_format = props['arg_format']
-    delete props['arg_format']
+    let url_scheme = 'shortcuts%3A%2F%2Frun-shortcut%3Fname%3DAuth%26input%3D'
 
     // 
-    console.log('url_scheme', url_scheme)
-    console.log('arg_format', arg_format)
     console.log('props', props)
 
     // append the data to the url_scheme
-    if (arg_format == 'qs') {
-        url_scheme = appendQuery(url_scheme, props)
-    } else {
-        // default is JSON format
-        url_scheme += `${encodeURI(JSON.stringify(props))}`
-    } 
+    // default is JSON format
+    url_scheme += `${encodeURI(JSON.stringify(props))}`
 
     // redirect
     console.log('url_scheme, final', url_scheme)
